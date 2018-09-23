e81899b06e7193fc0ae6bb99115a12c00c19b532
McGrg <mcgrg@yandex.ru>
2018-09-23 23:43:14 +0300

    Start

--- DigitParse/Program.cs
+++ DigitParse/Program.cs
@@ -10,6 +10,17 @@ namespace DigitParse
     {
         static void Main(string[] args)
         {
+            string text=Console.ReadLine();
+            Console.WriteLine($"This isdigit: {text}");
+            Console.ReadKey();
+            double digit = double.Parse(text);
+            double digitstart = Math.Truncate(digit);
+            double tenth = Math.Round(digit % digitstart, 2);
+            Console.WriteLine("{0} and {1}", digitstart, tenth);
+            double one = digitstart % 10;
+            Console.WriteLine($"This ones: {one}");
+
+            Console.ReadKey();
         }
     }
 }
