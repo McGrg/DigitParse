78f026cdbb3e0c4309bccf97f39388505fe4f63d
Tsepennikov Gregory <mcgrg@yandex.ru>
2018-09-25 16:34:47 +0300

    Working cleaned blueprint

--- DigitParse/Program.cs
+++ DigitParse/Program.cs
@@ -9,50 +9,41 @@ namespace DigitParse
     class Program
     {
         static bool thouthandsBool = false;
-        static bool millionsBool = false;
 
         static void Main(string[] args)
         {
+            StringBuilder result = new StringBuilder();
             string text = Console.ReadLine();
-            double digit = double.Parse(text);
-            double digitstart = Math.Truncate(digit);
-            double tenth = Math.Round(digit % digitstart, 2);
-            Console.WriteLine("{0} and {1}", digitstart, tenth);
-            double tens = digitstart % 100;
-            Console.WriteLine($"This tens: {tens}");
-            string tensString = null;
-            string handredsString = null;
-            string thouthandsString = null;
-            string hundredThouthandstring = null;
-            string millionsString = null;
-            tensString = tensStringParse(tens);
+            double temp = double.Parse(text);
 
-            Console.WriteLine($"This tensString: {tensString}");
-            Console.WriteLine(digitstart);
+            double digitstart = Math.Truncate(temp);
+            result.Append(Math.Round(temp % digitstart, 2).ToString());
 
-            digitstart = (digitstart - tens) / 100;
-            double hundreds = digitstart % 10;
+            result.Remove(0, 1);
 
-            handredsString = handredsStringParse(hundreds) + " " + tensString;
-            Console.WriteLine($"This tensString: {handredsString}");
+            temp = digitstart % 100;
+            result.Insert(0,tensStringParse(temp));
+            digitstart = (digitstart - temp) / 100;
 
-            digitstart = (digitstart - hundreds) / 10;
-            double thouthands = digitstart % 100;
+            temp = digitstart % 10;
+            result.Insert(0, handredsStringParse(temp));
+            digitstart = (digitstart - temp) / 10;
+
+            temp = digitstart % 100;
             thouthandsBool = true;
-            thouthandsString = tensStringParse(thouthands)+ " " + thouthandsWord(thouthands) + " " + handredsString;
+            result.Insert(0, thouthandsWord(temp));
+            result.Insert(0, tensStringParse(temp));
+            digitstart = (digitstart - temp) / 100;
 
-            digitstart = (digitstart - thouthands) / 100;
-            double hundredsThouthands = digitstart % 10;
-            Console.WriteLine($"This handredThouthand: {hundredsThouthands}");
+            temp = digitstart % 10;
+            result.Insert(0, handredsStringParse(temp));
+            digitstart = (digitstart - temp) / 10;
 
-            hundredThouthandstring = handredsStringParse(hundredsThouthands) + " " + thouthandsString;
-            Console.WriteLine($"This tensString: {hundredThouthandstring}");
+            temp = digitstart % 100;
+            result.Insert(0, millionsWord(temp));
+            result.Insert(0, tensStringParse(temp));
 
-            digitstart = (digitstart - hundredsThouthands) / 10;
-            double millions = digitstart % 100;
-            millionsBool = true;
-            millionsString = tensStringParse(millions) + " " + millionsWord(millions) + " " + hundredThouthandstring;
-            Console.WriteLine($"This millionsString: {millionsString}");
+            Console.WriteLine($"Result is: {result}");
 
             Console.ReadKey();
         }
@@ -60,80 +51,58 @@ namespace DigitParse
         static string onesStringParse(double toParse)
         {
             string stringResult = null;
-            switch (toParse)
+            if (thouthandsBool & toParse <= 2)
             {
-                case 1:
-                    stringResult = "один";
-                    break;
-                case 2:
-                    stringResult = "два";
-                    break;
-                case 3:
-                    stringResult = "три";
-                    break;
-                case 4:
-                    stringResult = "четыре";
-                    break;
-                case 5:
-                    stringResult = "пять";
-                    break;
-                case 6:
-                    stringResult = "шесть";
-                    break;
-                case 7:
-                    stringResult = "семь";
-                    break;
-                case 8:
-                    stringResult = "восемь";
-                    break;
-                case 9:
-                    stringResult = "девять";
-                    break;
-                default:
-                    stringResult = null;
-                    break;
+                switch (toParse)
+                {
+                    case 1:
+                        stringResult = " одна";
+                        break;
+                    case 2:
+                        stringResult = " две";
+                        break;
+                }
             }
-            return stringResult;
-        }
-        static string onesStringThouthandsParse(double toParse)
-        {
-            string stringResult = null;
-            switch (toParse)
+            else
             {
-                case 1:
-                    stringResult = "одна";
-                    break;
-                case 2:
-                    stringResult = "две";
-                    break;
-                case 3:
-                    stringResult = "три";
-                    break;
-                case 4:
-                    stringResult = "четыре";
-                    break;
-                case 5:
-                    stringResult = "пять";
-                    break;
-                case 6:
-                    stringResult = "шесть";
-                    break;
-                case 7:
-                    stringResult = "семь";
-                    break;
-                case 8:
-                    stringResult = "восемь";
-                    break;
-                case 9:
-                    stringResult = "девять";
-                    break;
-                default:
-                    stringResult = null;
-                    break;
+                switch (toParse)
+                {
+                    case 1:
+                        stringResult = " один";
+                        break;
+                    case 2:
+                        stringResult = " два";
+                        break;
+                    case 3:
+                        stringResult = " три";
+                        break;
+                    case 4:
+                        stringResult = " четыре";
+                        break;
+                    case 5:
+                        stringResult = " пять";
+                        break;
+                    case 6:
+                        stringResult = " шесть";
+                        break;
+                    case 7:
+                        stringResult = " семь";
+                        break;
+                    case 8:
+                        stringResult = " восемь";
+                        break;
+                    case 9:
+                        stringResult = " девять";
+                        break;
+                    default:
+                        stringResult = null;
+                        break;
+                }
             }
             thouthandsBool = false;
             return stringResult;
         }
+
         static string tensStringParse(double toParse)
         {
             string stringResult = null;
@@ -141,9 +110,6 @@ namespace DigitParse
             string stringOnesResult = null;
             if (toParse < 10)
             {
-                if (thouthandsBool)
-                    stringResult = onesStringThouthandsParse(toParse);
-                else
                     stringResult = onesStringParse(toParse);
             }
             else
@@ -152,37 +118,37 @@ namespace DigitParse
                 switch (toParse)
                 {
                     case 10:
-                        stringResult = "десять";
+                        stringResult = " десять";
                         break;
                     case 11:
-                        stringResult = "одинадцать";
+                        stringResult = " одинадцать";
                         break;
                     case 12:
-                        stringResult = "двенадцать";
+                        stringResult = " двенадцать";
                         break;
                     case 13:
-                        stringResult = "тринадцать";
+                        stringResult = " тринадцать";
                         break;
                     case 14:
-                        stringResult = "четырнадцать";
+                        stringResult = " четырнадцать";
                         break;
                     case 15:
-                        stringResult = "пятнадцать";
+                        stringResult = " пятнадцать";
                         break;
                     case 16:
-                        stringResult = "шестнадцать";
+                        stringResult = " шестнадцать";
                         break;
                     case 17:
-                        stringResult = "семнадцать";
+                        stringResult = " семнадцать";
                         break;
                     case 18:
-                        stringResult = "восемнадцать";
+                        stringResult = " восемнадцать";
                         break;
                     case 19:
-                        stringResult = "девятнадцать";
+                        stringResult = " девятнадцать";
                         break;
                     case 20:
-                        stringResult = "двадцать";
+                        stringResult = " двадцать";
                         break;
                     default:
                         stringResult = null;
@@ -193,75 +159,73 @@ namespace DigitParse
             {
                 double ones = toParse % 10;
                 double tens = (toParse - ones);
-                if (thouthandsBool)
-                    stringOnesResult = onesStringThouthandsParse(ones);
-                else
                     stringOnesResult = onesStringParse(ones);
                 switch (tens)
                 {
                     case 20:
-                        stringTenthResult = "двадцать";
+                        stringTenthResult = " двадцать";
                         break;
                     case 30:
-                        stringTenthResult = "тридцать";
+                        stringTenthResult = " тридцать";
                         break;
                     case 40:
-                        stringTenthResult = "сорок";
+                        stringTenthResult = " сорок";
                         break;
                     case 50:
-                        stringTenthResult = "пятьдесят";
+                        stringTenthResult = " пятьдесят";
                         break;
                     case 60:
-                        stringTenthResult = "шестьдесят";
+                        stringTenthResult = " шестьдесят";
                         break;
                     case 70:
-                        stringTenthResult = "семьдесят";
+                        stringTenthResult = " семьдесят";
                         break;
                     case 80:
-                        stringTenthResult = "восемьдесят";
+                        stringTenthResult = " восемьдесят";
                         break;
                     case 90:
-                        stringTenthResult = "девяносто";
+                        stringTenthResult = " девяносто";
                         break;
                     default:
                         stringTenthResult = null;
                         break;
                 }
-                stringResult = stringTenthResult + " " + stringOnesResult;
+                stringResult = stringTenthResult + stringOnesResult;
             }
             return stringResult;
         }
+
         static string handredsStringParse(double toParse)
         {
             string stringResult = null;
             switch (toParse)
             {
                 case 1:
-                    stringResult = "сто";
+                    stringResult = " сто";
                     break;
                 case 2:
-                    stringResult = "двести";
+                    stringResult = " двести";
                     break;
                 case 3:
-                    stringResult = "триста";
+                    stringResult = " триста";
                     break;
                 case 4:
-                    stringResult = "четыреста";
+                    stringResult = " четыреста";
                     break;
                 case 5:
-                    stringResult = "пятьсот";
+                    stringResult = " пятьсот";
                     break;
                 case 6:
-                    stringResult = "шестьсот";
+                    stringResult = " шестьсот";
                     break;
                 case 7:
-                    stringResult = "семьсот";
+                    stringResult = " семьсот";
                     break;
                 case 8:
-                    stringResult = "восемьсот";
+                    stringResult = " восемьсот";
                     break;
                 case 9:
-                    stringResult = "девятьсот";
+                    stringResult = " девятьсот";
                     break;
                 default:
                     stringResult = null;
@@ -269,56 +233,68 @@ namespace DigitParse
             }
             return stringResult;
         }
+
         static string thouthandsWord(double toParse, string stringResult=null)
         {
-            if (toParse < 20)
+            if (toParse > 0)
             {
-                switch (toParse)
+                if (toParse < 20)
                 {
-                    case 1:
-                        stringResult = "тысяча";
-                        break;
-                    case 2:
-                    case 3:
-                    case 4:
-                        stringResult = "тысячи";
-                        break;
-                    default:
-                        stringResult = "тысяч";
-                        break;
+                    switch (toParse)
+                    {
+                        case 1:
+                            stringResult = " тысяча";
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            stringResult = " тысячи";
+                            break;
+                        default:
+                            stringResult = " тысяч";
+                            break;
+                    }
+                }
+                else
+                {
+                    toParse = toParse % 10;
+                    stringResult = thouthandsWord(toParse, stringResult);
                 }
             }
             else
-            {
-                toParse = toParse % 10;
-                stringResult = thouthandsWord(toParse, stringResult);
-            }
+                stringResult = null;
             return stringResult;
         }
+
         static string millionsWord(double toParse, string stringResult = null)
         {
-            if (toParse < 20)
+            if (toParse > 0)
             {
-                switch (toParse)
+                if (toParse < 20)
                 {
-                    case 1:
-                        stringResult = "миллион";
-                        break;
-                    case 2:
-                    case 3:
-                    case 4:
-                        stringResult = "миллиона";
-                        break;
-                    default:
-                        stringResult = "миллионов";
-                        break;
+                    switch (toParse)
+                    {
+                        case 1:
+                            stringResult = " миллион";
+                            break;
+                        case 2:
+                        case 3:
+                        case 4:
+                            stringResult = " миллиона";
+                            break;
+                        default:
+                            stringResult = " миллионов";
+                            break;
+                    }
+                }
+                else
+                {
+                    toParse = toParse % 10;
+                    stringResult = millionsWord(toParse, stringResult);
                 }
             }
             else
-            {
-                toParse = toParse % 10;
-                stringResult = millionsWord(toParse, stringResult);
-            }
+                stringResult = null;
             return stringResult;
         }
     }
